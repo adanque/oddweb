5cec5eea42dda6713359db2a9596e9dfe1793a61
Alan Danque <adanque@gmail.com>
2025-10-01 21:36:20 -0500

    updated serve/server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();   
 //const port = process.env.PORT || 5000; 
-const PORT = 5000;
+//const PORT = 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.post('/api/chat', (req, res) => {
