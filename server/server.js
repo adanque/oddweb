ff243e4bf0dc32097bb1b68cfc47b3e093e74a80
Alan Danque <adanque@gmail.com>
2025-10-01 21:33:02 -0500

    update package.json for concurrently startup test

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();   
-const port = process.env.PORT || 5000; 
-//const PORT = 5000;
+//const port = process.env.PORT || 5000; 
+const PORT = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.post('/api/chat', (req, res) => {
